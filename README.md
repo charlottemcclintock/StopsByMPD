a7023cdb2c3d6a2ed7414fc258f1217fba37a26c
Charlotte McClintock <charlottemcclintock@MacBook-Pro.local>
2021-02-24 17:42:04 -0500

    add findings

--- README.md
+++ README.md
@@ -5,4 +5,13 @@ Data Source: [MPD Stop Data, January 1-June 30, 2020](https://mpdc.dc.gov/node/1
 
 # Findings
 
-87.1% of the youth stopped by MPD were Black. 
+  * 75% of the people stopped by MPD were Black. 
+  * 87.1% of the youth stopped by MPD were Black. 
+  *  Black people who were stopped were more likely to have a non-ticket stop (48.2% non-ticket stop, 48.7% ticket only) - evidence of racial profiling? White and Asian people who were stopped were more likely to actually get a ticket (86% of Asian people stopped and 78% of white people stopped got tickets). 
+
+|                           | Asian| Black| Hispanic| Multiple| NULL| Other| Unknown| White|
+|:--------------------------|-----:|-----:|--------:|--------:|----:|-----:|-------:|-----:|
+|Harbor                     |  0.27|  0.03|     0.64|     0.00|    0| 14.58|    0.00|  1.50|
+|Non-ticket Stop            | 12.03| 48.26|    39.54|    10.14|    0| 31.25|   70.38| 19.25|
+|Ticket and Non-ticket Stop |  1.07|  3.00|     1.93|     1.45|    0|  0.00|    2.78|  0.95|
+|Ticket Only                | 86.63| 48.70|    57.89|    88.41|  100| 54.17|   26.84| 78.30|
--- clean.R
+++ clean.R
@@ -1,33 +1,62 @@
 
+# c. mcclintock
+# defund mpd - now
+
+# ................................................................
+
+# load libraries
 library(tidyverse)
+library(RColorBrewer)
+library(knitr)
 
+# read in the data 
+stops1 <- read_csv("data/stops1.csv", na = "NULL")
+stops2 <- read_csv("data/stops2.csv", na = "NULL")
 
-stops1 <- read_csv("data/stops1.csv")
-stops2 <- read_csv("data/stops2.csv")
+# ................................................................
 
-
+# join time periods together
 stops <- full_join(stops1, stops2, by=names(stops1))
 
+# categorical duration values
 stops$duration_cut <- cut(stops$stop_duration_minutes, 
                           breaks = c(1,5,20,60, 120, 360), 
                           include.lowest = T)
 
+# create subset for youth
 youth <- subset(stops, age=="Juvenile")
 
+# age to numeric
 stops$age <- as.numeric(stops$age)
 
-ggplot(stops, aes(stop_district, fill=race_ethnicity)) + geom_bar(position="fill")
+# race by stop district
+ggplot(stops, aes(stop_district, fill=race_ethnicity)) + 
+  geom_bar(position="fill") + 
+  scale_fill_manual(values=brewer.pal(8, "Set2"))
 
+# stop duration by race
 ggplot(stops, aes(duration_cut, fill=race_ethnicity)) + geom_bar(position="dodge")
 
+# and median and mean
 stops %>% 
   group_by(race_ethnicity) %>% 
   summarize(median=median(stop_duration_minutes, na.rm = T), 
             mean=mean(stop_duration_minutes, na.rm=T))
 
+# youth stop race demographics
+table(youth$race_ethnicity) %>% prop.table()
 
-table(youth$race_ethnicity)
+# overall stop race demographics
+table(stops$race_ethnicity) %>% prop.table()
 
+
+# age by race
 ggplot(stops, aes(age, fill=race_ethnicity)) + geom_density(alpha=0.6)
+
+# stop type by race
+kable(100*prop.table(table(stops$stop_type, stops$race_ethnicity), margin=2), digits = 2)
+# black citizens who were stopped were more likely to have a non-ticket stop 
+# evidence of racial profiling
+
 
 
